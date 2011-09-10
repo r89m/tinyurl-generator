c3ed4d6af865c69f7707c7c496906e823465db2d
shaggy.the.sheep@googlemail.com <shaggy.the.sheep@googlemail.com@1e591c69-0b05-5ac3-81bc-5ac42a1ae094>
2011-09-10 16:39:53 +0000

    Cleaning up

--- branches/v2.5.1/defaults/preferences/pref.js
+++
@@ -1,7 +0,0 @@
-pref("extensions.tinyurlgen.createpreviewbydefault", false);
-pref("extensions.tinyurlgen.showcontextoptionforpage", true);
-pref("extensions.tinyurlgen.showcontextoptionforlinks", true);
-pref("extensions.tinyurlgen.shortcutenabled", false);
-pref("extensions.tinyurlgen.shortcutmodifier", '');
-pref("extensions.tinyurlgen.shortcutkey", '');
-pref("extensions.tinyurlgen.firstrun", true);
