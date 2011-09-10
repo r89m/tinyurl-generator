cda8b544531dba2dac8991fff36936f9a7b16a15
shaggy.the.sheep@googlemail.com <shaggy.the.sheep@googlemail.com@1e591c69-0b05-5ac3-81bc-5ac42a1ae094>
2011-09-10 15:44:17 +0000

    Removed no-longer-needed preference indicating whether or not show the icon in the addon bar

--- trunk/defaults/preferences/pref.js
+++ trunk/defaults/preferences/pref.js
@@ -1,7 +1,6 @@
 pref("extensions.tinyurlgen.createpreviewbydefault", false);
 pref("extensions.tinyurlgen.showcontextoptionforpage", true);
 pref("extensions.tinyurlgen.showcontextoptionforlinks", true);
-pref("extensions.tinyurlgen.iconinaddonbar", true);
 pref("extensions.tinyurlgen.shortcutenabled", false);
 pref("extensions.tinyurlgen.shortcutmodifier", '');
 pref("extensions.tinyurlgen.shortcutkey", '');
